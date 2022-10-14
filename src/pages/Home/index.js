ea9f338b5f5e8e03a3f93ec3232e8cabb94b3f73
Leandro Filie <leandro.gfilie@gmail.com>
2022-10-14 00:08:04 -0300

    refactor(Home): adaptation to the new Header component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import {
-  Container, Banner, TitleSection, ContainerProgram, ContainerNotice,
+  Banner, TitleSection, ContainerProgram, ContainerNotice, Button,
 } from './styles';
 
 import { Footer } from '../../components/Footer';
@@ -11,84 +12,90 @@ import PlusIcon from '../../assets/plus.png';
 export default function Home() {
   return (
     <>
-      <Container>
-        <Header />
+      <Header>
+        <li>
+          <a href="#notice">Editais</a>
+          <span />
+        </li>
+        <li>
+          <a href="#program">O Programa</a>
+          <span />
+        </li>
+        <Link to="/login"><Button type="button">Área Administrativa</Button></Link>
+      </Header>
 
-        <Banner>
-          <div className="banner-text">
-            <h1>Programa de Desenvolvimento de Inteligência Artificial em Saúde</h1>
-          </div>
-        </Banner>
+      <Banner>
+        <div className="banner-text">
+          <h1>Programa de Desenvolvimento de Inteligência Artificial em Saúde</h1>
+        </div>
+      </Banner>
 
-        <ContainerNotice id="notice">
-          <TitleSection>
-            <img src={PlusIcon} alt="Icon" />
-            <h2>Editais de Linhas de Pesquisa</h2>
-          </TitleSection>
+      <ContainerNotice id="notice">
+        <TitleSection>
+          <img src={PlusIcon} alt="Icon" />
+          <h2>Editais de Linhas de Pesquisa</h2>
+        </TitleSection>
 
-          <Edital name="Extração de relações temporais em textos clínicos" isOpened />
+        <Edital name="Extração de relações temporais em textos clínicos" isOpened />
 
-          <Edital name="Extração de relações temporais em textos clínicos" isOpened />
+        <Edital name="Extração de relações temporais em textos clínicos" isOpened />
 
-          <Edital name="Extração de relações temporais em textos clínicos" isOpened={false} />
+        <Edital name="Extração de relações temporais em textos clínicos" isOpened={false} />
 
-          <Edital name="Extração de relações temporais em textos clínicos" isOpened />
-        </ContainerNotice>
+        <Edital name="Extração de relações temporais em textos clínicos" isOpened />
+      </ContainerNotice>
 
-        <ContainerProgram id="program">
-          <TitleSection>
-            <img src={PlusIcon} alt="Icon" />
-            <h2>O Programa</h2>
-          </TitleSection>
+      <ContainerProgram id="program">
+        <TitleSection>
+          <img src={PlusIcon} alt="Icon" />
+          <h2>O Programa</h2>
+        </TitleSection>
 
-          <div className="content">
-            <div className="objective">
-              <p>
-                <img src={PlusIcon} alt="Item" />
-                Aproximar a organização do ambiente de pesquisa acadêmico
-              </p>
-              <p>
-                <img src={PlusIcon} alt="Item" />
-                Aumentar a contratação e retenção de talentos de IA em saúde
-              </p>
-              <p>
-                <img src={PlusIcon} alt="Item" />
-                Demonstrar autoridade no tema através de publicações científicas
-              </p>
-              <p>
-                <img src={PlusIcon} alt="Item" />
-                Desenvolver tecnologias que possam ser acopladas ao pipeline atual
-                de produtos ou que possibilitem novos caminhos de geração de valor
-              </p>
-            </div>
-
-            <div className="requirements">
-              <p>
-                <img src={PlusIcon} alt="Item" />
-                Estudantes de Iniciação Científica, Mestrado, Doutorado ou Pós-doutorado
-              </p>
-
-              <p>
-                <img src={PlusIcon} alt="Item" />
-                Interesse nos temas apresentados
-              </p>
-
-              <p>
-                <img src={PlusIcon} alt="Item" />
-                Recebimento de bolsa
-              </p>
-
-              <p>
-                <img src={PlusIcon} alt="Item" />
-                Se tornará parte da equipe de NLP da Comsentimento
-              </p>
-            </div>
+        <div className="content">
+          <div className="objective">
+            <p>
+              <img src={PlusIcon} alt="Item" />
+              Aproximar a organização do ambiente de pesquisa acadêmico
+            </p>
+            <p>
+              <img src={PlusIcon} alt="Item" />
+              Aumentar a contratação e retenção de talentos de IA em saúde
+            </p>
+            <p>
+              <img src={PlusIcon} alt="Item" />
+              Demonstrar autoridade no tema através de publicações científicas
+            </p>
+            <p>
+              <img src={PlusIcon} alt="Item" />
+              Desenvolver tecnologias que possam ser acopladas ao pipeline atual
+              de produtos ou que possibilitem novos caminhos de geração de valor
+            </p>
           </div>
 
-        </ContainerProgram>
+          <div className="requirements">
+            <p>
+              <img src={PlusIcon} alt="Item" />
+              Estudantes de Iniciação Científica, Mestrado, Doutorado ou Pós-doutorado
+            </p>
 
-      </Container>
+            <p>
+              <img src={PlusIcon} alt="Item" />
+              Interesse nos temas apresentados
+            </p>
 
+            <p>
+              <img src={PlusIcon} alt="Item" />
+              Recebimento de bolsa
+            </p>
+
+            <p>
+              <img src={PlusIcon} alt="Item" />
+              Se tornará parte da equipe de NLP da Comsentimento
+            </p>
+          </div>
+        </div>
+
+      </ContainerProgram>
       <Footer />
     </>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,8 +2,22 @@ import styled from 'styled-components';
 
 import BannerImage from '../../assets/banner.png';
 
-export const Container = styled.main`
+export const Button = styled.button`
+  padding: 0.75rem 1rem;
+  background: transparent;
 
+  border: solid 2px ${({ theme }) => theme.colors.primary};
+  border-radius: 4px;
+
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: 1rem;
+  transition: all 0.2s;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.gray[50]};
+    background: ${({ theme }) => theme.colors.primary};
+    transition: all 0.2s;
+  }
 `;
 
 export const Banner = styled.section`

b995a0d6fe4c8df9e08847027dc0d4f201d3f765
Leandro Filie <leandro.gfilie@gmail.com>
2022-10-14 00:06:46 -0300

    refactor(Header): header for most pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,15 @@
+/* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
-import { Button, Container, Menu } from './styles';
+import { Container, Menu } from './styles';
 
 import Logo from '../../assets/logo-horizontal-01.png';
 
-export default function Header() {
+export default function Header({ children }) {
   return (
     <Container>
       <Link to="/"><img src={Logo} alt="Comsentimento" /></Link>
       <Menu>
-        <li>
-          <a href="#notice">Editais</a>
-          <span />
-        </li>
-        <li>
-          <a href="#program">O Programa</a>
-          <span />
-        </li>
-        <Link to="/login"><Button type="button">Área Administrativa</Button></Link>
+        { children }
       </Menu>
     </Container>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -17,7 +17,7 @@ export const Container = styled.header`
 export const Menu = styled.ul`
   display: flex;
   align-items: center;
-  gap: 3rem;
+  gap: 1.5rem;
 
   li {
     font-size: 1.075rem;
@@ -45,23 +45,5 @@ export const Menu = styled.ul`
 
   @media (max-width: 650px) {
     display: none;
-  }
-`;
-
-export const Button = styled.button`
-  padding: 0.75rem 1rem;
-  background: transparent;
-
-  border: solid 2px ${({ theme }) => theme.colors.primary};
-  border-radius: 4px;
-
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: 1rem;
-  transition: all 0.2s;
-
-  &:hover {
-    color: ${({ theme }) => theme.colors.gray[50]};
-    background: ${({ theme }) => theme.colors.primary};
-    transition: all 0.2s;
   }
 `;

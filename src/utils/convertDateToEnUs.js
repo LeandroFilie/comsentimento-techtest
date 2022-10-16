e2cc65455eb18288ee5a4c0d1a8c9ab7c2cd513a
Leandro Filie <leandro.gfilie@gmail.com>
2022-10-16 12:08:12 -0300

    fix(convertDate): fixed receive null in function)

--- src/utils/convertDateToEnUs.js
+++ src/utils/convertDateToEnUs.js
@@ -1,9 +1,9 @@
 export default function convertDateToEnUs(date) {
+  if (!date) return null;
+
   const dateArray = date.split('/');
 
   const dateFormatted = `${dateArray[2]}-${dateArray[1].padStart(2, 0)}-${dateArray[0].padStart(2, 0)}`;
-
-  console.log(dateFormatted);
 
   return dateFormatted;
 }

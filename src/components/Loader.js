ddf496b9c370f4b06f94d11ea7e70735611122fc
Leandro Filie <leandro.gfilie@gmail.com>
2022-10-16 23:44:15 -0300

    feat(Loader): added color and size props receive

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,17 +1,29 @@
-export default function Loader() {
+import PropTypes from 'prop-types';
+
+export default function Loader({ color, size }) {
   return (
     <svg
       style={{
         margin: '0 auto', display: 'block', shapeRendering: 'auto',
       }}
-      width="150px"
-      height="150px"
+      width={`${size}px`}
+      height={`${size}px`}
       viewBox="0 0 100 100"
       preserveAspectRatio="xMidYMid"
     >
-      <circle cx="50" cy="50" fill="none" stroke="#604FFF" strokeWidth="6" r="32" strokeDasharray="150.79644737231007 52.26548245743669">
+      <circle cx="50" cy="50" fill="none" stroke={color} strokeWidth="6" r="32" strokeDasharray="150.79644737231007 52.26548245743669">
         <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1" />
       </circle>
     </svg>
   );
 }
+
+Loader.propTypes = {
+  color: PropTypes.string,
+  size: PropTypes.string,
+};
+
+Loader.defaultProps = {
+  color: '#604FFF',
+  size: '150',
+};
